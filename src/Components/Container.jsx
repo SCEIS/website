da4dd2fd2b9d87e1adb8e4a41f5e347933f9dc20
Kevin <kevcespedesarancibia87@gmail.com>
2023-09-05 11:40:23 -0400

    Add sx custom option to Container AND Container to enrollment

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material'
 function Container(props){
     let sxContainer = {        
         borderRadius: '16px',
+        ...props.sx
     };
     return (<Box sx={sxContainer}>
         {props.children}
--- src/Views/Enrollment.jsx
+++ src/Views/Enrollment.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Box, Typography, Button, Alert } from '@mui/material'
-import Container from '@mui/material/Container'
+import { Container } from '../Components/Container';
 import TextField from '@mui/material/TextField';
 
 function EnrollmentForm(){
@@ -26,48 +26,65 @@ function EnrollmentForm(){
     const emailRegex = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
     const invalidEmailAlert = (<Alert severity="error">Introduce una dirección de correo valida</Alert>);
     
-    const sxInput = {my:2}
-    return (<Box component="form">
-        <TextField fullWidth
-            sx={sxInput}
-            id="name"
-            label="Nombre(s)"
-            name='name'
-            value={enrollmentForm.name}
-            onChange={handleChange}
-            required
-        />
-        <TextField fullWidth
-            sx={sxInput}
-            id="lastName"
-            label="Apellido(s)"
-            name='lastName'
-            value={enrollmentForm.lastName}
-            onChange={handleChange}
-            required
-        />
-        <TextField fullWidth
-            sx={sxInput}
-            id="email"
-            label="Email"
-            type='email'
-            name='email'
-            value={enrollmentForm.email}
-            onChange={handleChange}
-            required
-        />
-        {emailError ? invalidEmailAlert : null}
-        <Button type="submit">Enviar</Button>
-    </Box>);
+    const sxInput = {marginBottom:2}
+    
+    const containerSx = {
+        width:0.9,
+        padding: 3,
+        border:1
+    };
+
+    return (
+    <Container sx={containerSx}>
+        <Typography variant='h5' sx={{marginBottom:2}}>Registro</Typography>
+        <Box component="form">
+            <TextField fullWidth
+                sx={sxInput}
+                id="name"
+                label="Nombre(s)"
+                name='name'
+                value={enrollmentForm.name}
+                onChange={handleChange}
+                required
+            />
+            <TextField fullWidth
+                sx={sxInput}
+                id="lastName"
+                label="Apellido(s)"
+                name='lastName'
+                value={enrollmentForm.lastName}
+                onChange={handleChange}
+                required
+            />
+            <TextField fullWidth
+                sx={sxInput}
+                id="email"
+                label="Email"
+                type='email'
+                name='email'
+                value={enrollmentForm.email}
+                onChange={handleChange}
+                required
+            />
+            {emailError ? invalidEmailAlert : null}
+            <Button type="submit">Enviar</Button>
+        </Box>
+    </Container>
+    );
 }
 
 function Enrollment(){
+    const containerSx = {
+        width:{xs:0.9,sm:0.9,md:0.7,lg:0.6,xl:0.6},
+    };
     const [showForm,setShowForm]=useState(false);
     let enrollmentForm = <EnrollmentForm/>;
-    return (<Container>
+    return (<Box sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <Container sx={containerSx}>
         <Typography variant='h4'>Quiero ser parte!</Typography>
         <Button onClick={(e)=>{setShowForm(!showForm)}}>Unirme a la SCEIS</Button>
         {showForm ? enrollmentForm : null}
-    </Container>);
+        </Container>
+    </Box>);
 }
 export {Enrollment}
